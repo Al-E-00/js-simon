ecb27851c79136fc6257eac483e740a09c4948a9
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-31 16:59:40 +0200

    Problem with comparison of the arrays

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     2.  Start 30 sec timer
     3.  After 30 sec, hide the numbers
     4.  The user must enter, once a time, the numbers that previously have seen, with a prompt().
-    5.  After the 5 numbers have been entered, the software tells how many and with of the numbers have been identified
+    5.  After the 5 numbers have been entered, the software tells how many and witch of the numbers have been identified
 */
 
 //DOM 
@@ -13,18 +13,55 @@ const showNumbers = document.querySelector('.show-numbers')
 
 
 const generatedNumbers = [];
-
+const userNumbersEntered = [];
+const correctNumbers = [];
 
     for (let i = 0; i < 5; i++ ) {
-        let newNumberGenerator = Math.floor(Math.random() * 10) + 1;
+        let newNumberGenerator = Math.floor(Math.random() * 10);
 
         // with this loop the numbers are all differents
         if(generatedNumbers.includes(newNumberGenerator)) {
             i--;
         } else {
             generatedNumbers.push(newNumberGenerator)
-        }    
+            showNumbers.append(` ${generatedNumbers[i]} -`);
+        }   
+        
     }
 
-document.showNumbers.innerHTML = '<span> ${generatedNumbers} </span>'
+    // funciton -> after 30 sec hide the numbers
+    setTimeout (hideNumbers, 1000);
 
+        function hideNumbers () {
+            showNumbers.remove(` ${generatedNumbers} -`);
+        }
+    
+    
+    //function -> afret 32 sec ask to the user to enter the numbers
+    setTimeout (userNumbers, 2000);
+
+    function userNumbers () {
+        
+        // let enter the prompt 5 times
+        for (let i = 0; i < 5; i++) {
+            let userNumbersPrompt = parseInt(prompt("Enter the numbers, one at time,  you have seen previously"));
+            
+            userNumbersEntered.push(userNumbersPrompt);
+        }
+    }
+
+    //function -> compare the two values and push only the correct ones
+    // set timeout for this function that starts when the previous ones have finished
+
+    setTimeout (checkAnswer, 3000);
+
+    function checkAnswer () {
+        if (userNumbersEntered === generatedNumbers) {
+            correctNumbers.push(userNumbersEntered[i]);
+    }
+    }
+
+
+    console.log(generatedNumbers);
+    console.log(userNumbersEntered);
+    console.log(correctNumbers);
