de5d3963b017f0c79399580980fea1763be36f3a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-31 16:13:54 +0200

    generate 5 random numbers, everyone different

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+/* 
+    1.  Visualize on the page(html) 5 random numbers.
+    2.  Start 30 sec timer
+    3.  After 30 sec, hide the numbers
+    4.  The user must enter, once a time, the numbers that previously have seen, with a prompt().
+    5.  After the 5 numbers have been entered, the software tells how many and with of the numbers have been identified
+*/
+
+//DOM 
+// push the numbers inside the html document
+const showNumbers = document.querySelector('.show-numbers')
+
+
+const generatedNumbers = [];
+
+
+    for (let i = 0; i < 5; i++ ) {
+        let newNumberGenerator = Math.floor(Math.random() * 10) + 1;
+
+        // with this loop the numbers are all differents
+        if(generatedNumbers.includes(newNumberGenerator)) {
+            i--;
+        } else {
+            generatedNumbers.push(newNumberGenerator)
+        }    
+    }
+
